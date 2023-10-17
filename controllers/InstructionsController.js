62915e887ca3ee8ffc86d5b2b3d8ecf6226398d7
khloeli <chloelihuimin@gmail.com>
2023-10-17 12:45:04 +0800

    add photo code part 1

--- controllers/InstructionsController.js
+++ controllers/InstructionsController.js
@@ -6,4 +6,39 @@ class InstructionsController extends BaseController {
   }
 }
 
+// update photo
+  async updatePhoto(req, res) {
+    try {
+      let itineraryToAdd = req.body;
+      const { userId, itineraryId } = req.params;
+      // Find the existing itinerary
+      let itineraryToEdit = await this.model.findByPk(itineraryId);
+      if (!itineraryToEdit) {
+        return res
+          .status(404)
+          .json({ error: true, msg: "Itinerary not found" });
+      }
+      console.log("itineraryToEdit", itineraryToEdit);
+      const userItineraryRecord = await this.user_itinerariesModel.findOne({
+        where: {
+          userId: userId,
+          itineraryId: itineraryId,
+        },
+      });
+      console.log("userItineraryRecord", userItineraryRecord);
+
+      // Check if the user is the creator
+      if (!userItineraryRecord.isCreator) {
+        throw new Error(
+          "Only the creator can edit new activity in this itinerary"
+        );
+      }
+      await itineraryToEdit.update(itineraryToAdd);
+
+      return res.json(itineraryToEdit);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err.message });
+    }
+  
+
 module.exports = InstructionsController;
