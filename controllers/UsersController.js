1ab3a86cb665e740f6079f46c2b4a595f992e70c
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-10-18 21:55:40 +0800

    Change implementation of handling first time user login

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -24,27 +24,6 @@ class UsersController extends BaseController {
     }
   }
 
-  // get user's login count
-  async getLoginCount(req, res) {
-    const { email } = req.params;
-
-    var options = {
-      method: "GET",
-      url: "https://dev-ou073hqmuqdme5yd.us.auth0.com/api/v2/users",
-      params: { q: `email:${email}`, search_engine: "v3" },
-      headers: { Authorization: `Bearer ${process.env.MANAGEMENT_API_KEY}` },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        return res.json(response.data);
-      })
-      .catch(function (err) {
-        return res.status(400).json({ error: true, msg: err });
-      });
-  }
-
   // create user with form information
   async addUser(req, res) {
     const { name, email, cusinePreferences, dietaryRestrictions } = req.body;
--- routers/UsersRouter.js
+++ routers/UsersRouter.js
@@ -8,10 +8,6 @@ class UsersController {
     const router = this.express.Router();
     router.get("/", this.controller.getAll.bind(this.controller));
     router.get(
-      "/management/:email",
-      this.controller.getLoginCount.bind(this.controller)
-    );
-    router.get(
       "/:email",
       this.controller.getFirstTimeLoginStatus.bind(this.controller)
     );
